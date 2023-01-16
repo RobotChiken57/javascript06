c7fca80e3e01d6d2bed6b1d0fb90cbee8a9091ff
RobotChiken57 <99732056+RobotChiken57@users.noreply.github.com>
2023-01-16 09:37:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+//1
+//let getInput = document.querySelectorAll('.task');
+//let getP = document.querySelector('#test');
+//for (let i = 0; i < getInput.length; i++) {
+//   getInput[i].addEventListener('blur', func5);
+//}
+//function func5() {
+//   getP.innerHTML = getP.innerHTML + this.value;
+//}
+//2
+//let elems = document.querySelectorAll('input');
+//for (let i = 0; i<elems.length; i++){
+//    elems[i].addEventListener('click', func);
+//}
+
+//function func() {
+//    alert(this.value);
+//    this.removeEventListener('click', func)
+//}
+
+//3
+//let getP = document.querySelectorAll('p');
+
+//for (let i = 0; i < getP.length; i++) {
+//   getP[i].addEventListener('click', func);
+//}
+
+//function func () {
+//    this.innerHTML = Number(this.innerHTML) * Number(this.innerHTML);
+//}
+//4
+//let getInput = document.querySelectorAll('input');
+
+//for (let i = 0; i<getInput.length; i++) {
+//    getInput[i].addEventListener('blur', func);
+//}
+
+//function func() {
+//    if (Number(this.dataset.length) === Number(this.value.length)) {
+//        this.style.border = '3px solid green';
+//    } else {
+//        this.style.border = '3px solid red';
+//    }
+//}
